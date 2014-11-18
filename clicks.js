262688880ab7de3e4e67762dc7bcbb576528cda8
DEtheFree <diegoesparza@sandybrown-output.local>
2014-11-17 16:53:28 -0800

    random crafp

--- clicks.js
+++ clicks.js
@@ -1,2 +1,28 @@
 /*global $*/
 /*jslint sloppy:true, browser: true*/
+$("h1.page-title").text("My code runs!");
+$(window).on('mousemove', function (e) {
+var mouseX=e.pageX;
+    var mouseY=e.pageY;
+   var imageX= parseInt($('img.logo').css("left"));
+    
+    var imageY= parseInt($('img.logo').css("top"));
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    if (mouseX+100 >imageX && mouseY+100 >imageY){
+    
+    $('img.logo').css({left: mouseX+100,
+                       top: mouseY+100});}
+});
+
